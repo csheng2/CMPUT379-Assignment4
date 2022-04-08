eb697234151f66b4b65bd4b52e821292e9f7128e
Student <csheng2@ualberta.ca>
2022-04-08 01:27:24 -0400

    added job parsing

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 struct Resource {
     std::string name;
-    int max_value;
+    int value;
     int in_use = 0;
 };
 
@@ -20,14 +20,14 @@ int JOBS_LENGTH = 0;
 struct Job* JOBS = new Job[JOBS_LENGTH];
 
 void growGlobalJobsList(){
-    Resource* temp = new Resource[RESOURCES_LENGTH + 1];
-    std::copy(RESOURCES, RESOURCES + RESOURCES_LENGTH, temp);
-    delete [] RESOURCES;
-    RESOURCES = temp;
-    RESOURCES_LENGTH++;
+    Job* temp = new Job[JOBS_LENGTH + 1];
+    std::copy(JOBS, JOBS + JOBS_LENGTH, temp);
+    delete [] JOBS;
+    JOBS = temp;
+    JOBS_LENGTH++;
 }
 
-void growJobResourcesList(Job job){
+void growJobResourcesList(Job& job){
     Resource* temp = new Resource[job.resources_length + 1];
     std::copy(job.resources, job.resources + job.resources_length, temp);
     delete [] job.resources;
@@ -45,10 +45,27 @@ void growGlobalResourcesList(){
 
 void printInitializedResources() {
     printf("==========================\n");
-    printf("Initialized resources:");
+    printf("Initialized resources:\n");
 
     for (int x = 0; x < RESOURCES_LENGTH; x++){
-        std::cout << "name: " << RESOURCES[x].name << " value: " << RESOURCES[x].max_value << std::endl;
+        std::cout << "name: " << RESOURCES[x].name << " value: " << RESOURCES[x].value << std::endl;
+    }
+
+    printf("==========================\n");    
+}
+
+void printParsedJobs() {
+    printf("==========================\n");
+    printf("Parsed jobs from file:\n");
+
+    for (int x = 0; x < JOBS_LENGTH; x++){
+        std::cout << "jobName: " << JOBS[x].job_name << " | busyTime: " << JOBS[x].busy_time << " | idleTime: " << JOBS[x].idle_time << std::endl;
+        std::cout << "\tresources:";
+
+        for (int i = 0; i < JOBS[x].resources_length; i++){
+            std::cout << " | name: " << JOBS[x].resources[i].name << " value: " << JOBS[x].resources[i].value;
+        }
+        printf("\n\n");
     }
 
     printf("==========================\n");    
@@ -62,24 +79,30 @@ void initResources(std::string resource_string){
 
     try{
         while ((pos = resource_string.find(space_delimiter)) != std::string::npos) {
+            // remove extra white spaces
+            if(resource_string[pos - 1] == ' '){
+                resource_string.erase(0, pos + space_delimiter.length());
+                continue;
+            }
+
             growGlobalResourcesList();
 
             token = resource_string.substr(0, pos);
             resource_string.erase(0, pos + space_delimiter.length());
 
-            pos = token.find(colon_delimiter);;
+            pos = token.find(colon_delimiter);
             RESOURCES[RESOURCES_LENGTH - 1].name = token.substr(0, pos);
             token.erase(0, pos + colon_delimiter.length());
-            RESOURCES[RESOURCES_LENGTH - 1].max_value = stoi(token.substr(0, pos));
+            RESOURCES[RESOURCES_LENGTH - 1].value = stoi(token);
         }
         
         // last resource:
         growGlobalResourcesList();
         token = resource_string;
-        pos = token.find(colon_delimiter);;
+        pos = token.find(colon_delimiter);
         RESOURCES[RESOURCES_LENGTH - 1].name = token.substr(0, pos);
         token.erase(0, pos + colon_delimiter.length());
-        RESOURCES[RESOURCES_LENGTH - 1].max_value = stoi(token.substr(0, pos));
+        RESOURCES[RESOURCES_LENGTH - 1].value = stoi(token);
 
         printInitializedResources();
     } catch(const std::exception& e) {
@@ -88,8 +111,60 @@ void initResources(std::string resource_string){
     }
 }
 
-void initJob(std::string job_string){
-    growGlobalJobsList();
+Job initJob(std::string job_string){
+    struct Job job;
+    std::string space_delimiter = " ";
+    std::string colon_delimiter = ":";
+    size_t pos = 0;
+    std::string token;
+    int index;
+
+    try{
+        // Job Name
+        pos = job_string.find(space_delimiter);
+        job.job_name = job_string.substr(0, pos);
+        job_string.erase(0, pos + space_delimiter.length());
+
+        // Busy Time
+        pos = job_string.find(space_delimiter);
+        job.busy_time = stoi(job_string.substr(0, pos));
+        job_string.erase(0, pos + space_delimiter.length());
+
+        // Idle Time
+        pos = job_string.find(space_delimiter);
+        job.idle_time = stoi(job_string.substr(0, pos));
+        job_string.erase(0, pos + space_delimiter.length());
+
+        // Resources
+        while ((pos = job_string.find(space_delimiter)) != std::string::npos) {
+            growJobResourcesList(job);
+            index = job.resources_length - 1;
+
+            token = job_string.substr(0, pos);
+            job_string.erase(0, pos + space_delimiter.length());
+
+            pos = token.find(colon_delimiter);
+            job.resources[index].name = token.substr(0, pos);
+            token.erase(0, pos + colon_delimiter.length());
+
+            job.resources[index].value = stoi(token);
+        }
+        
+        // last resource:
+        growJobResourcesList(job);
+        index = job.resources_length - 1;
+        token = job_string;
+
+        pos = token.find(colon_delimiter);
+        job.resources[index].name = token.substr(0, pos);
+        token.erase(0, pos + colon_delimiter.length());
+        job.resources[index].value = stoi(token);
+
+        return job;
+    } catch(const std::exception& e) {
+        std::cout << "initJob - Error: " << &e << std::endl;
+        exit(0);
+    }
 }
 
 int processInputFile(std::string file_name){
@@ -120,9 +195,14 @@ int processInputFile(std::string file_name){
             // erase "job " from the line before passing it to the function
             std::string keyword = "job ";
             line.erase(0, keyword.length());            
-            initJob(line);
+            struct Job job = initJob(line);
+
+            growGlobalJobsList();
+            JOBS[JOBS_LENGTH - 1] = job;
         }
     }
+
+    printParsedJobs();
 
     newfile.close();
     return -2;
